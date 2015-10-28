dececfa006395fe0dfeb6cc644ec8e9b21cc038d
VulcanStorm <vulcanstorm98@gmail.com>
2015-10-28 22:21:00 +0000

    Working test scene
    needs a lot of work. but its a prototype thing you can expand on

---
+++ Assets/Scripts/PlayerMove.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+
+var jumpSpeed : float = 10;
+var moveSpeed : float = 3;
+
+var walkInput : float = 0;
+
+var myRigidbody : Rigidbody = null;
+
+// called when the object is loaded
+function Start () {
+	myRigidbody = this.GetComponent(Rigidbody);
+}
+
+function Update () {
+
+}
+
+
+function FixedUpdate () {
+		
+		// check for jump
+		if(Input.GetKeyUp(KeyCode.Space)){
+			// we are jumping, so add some force upwards to move us
+			myRigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+		}
+		
+		// get the input of the player
+		walkInput = Input.GetAxis("Horizontal");
+		
+		// move forward
+		transform.Translate(Vector3.forward * walkInput * moveSpeed * Time.deltaTime);
+}
